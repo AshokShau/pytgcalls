3e0b94e0294f90a5897c87bda37f110906a60429
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-01-03 17:55:44 +0530

    Fix UpdateGroupCall chat_id attribute error by supporting peer and cache fallback

--- pytgcalls/mtproto/telethon_client.py
+++ pytgcalls/mtproto/telethon_client.py
@@ -204,11 +204,18 @@ class TelethonClient(BridgedClient):
                 update,
                 UpdateGroupCall,
             ):
-                chat_id = self.chat_id(
-                    await self._get_entity_group(
-                        update.chat_id,
-                    ),
-                )
+                if hasattr(update, 'chat_id'):
+                    chat_id = self.chat_id(
+                        await self._get_entity_group(
+                            update.chat_id,
+                        ),
+                    )
+                else:
+                    if getattr(update, 'peer', None):
+                        chat_id = self.chat_id(update.peer)
+                    else:
+                        chat_id = self._cache.get_chat_id(update.call.id)
+
                 if isinstance(
                     update.call,
                     GroupCall,
